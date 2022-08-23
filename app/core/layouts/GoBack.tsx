4a7b0efc62d049091c5653c529250385128ad36a
dmojarrot <a01638460@itesm.mx>
2022-08-23 12:50:33 -0500

    Added create path and some of their components (misses project form component)

---
+++ app/core/layouts/GoBack.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+interface IProps {
+  title: String;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+}
+
+function GoBack({ title, onClick }: IProps) {
+  return (
+    <>
+      <Wrapper className="wrapper__back">
+        <div className="wrapper__back--icon" onClick={onClick} />
+        <div className="wrapper__back--text">{title}</div>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  margin-left: 12px;
+  display: flex;
+  height: 51px;
+  margin-bottom: 35px;
+  .wrapper__back--icon {
+    width: 23px;
+    height: 23px;
+    background-size: contain;
+    background-image: url(/arrow-back.png);
+    cursor: pointer;
+  }
+  .wrapper__back--text {
+    color: #000000;
+    font-family: Poppins;
+    font-size: 18px;
+    letter-spacing: 0;
+    line-height: 27px;
+    margin-left: 14px;
+  }
+`;
+
+export default GoBack;
---
+++ app/routes/projects/create/index.tsx
@@ -0,0 +1,31 @@
+import Header from "app/core/layouts/Header";
+import GoBack from "app/core/layouts/GoBack";
+import { useNavigate } from "@remix-run/react";
+
+
+
+const NewProjectPage = () => {
+    const navigate = useNavigate();
+  return (
+    <div>
+      <Header title="Create your proposal" />
+      <div className="wrapper">
+        <h1 className="form__center-text">Create your proposal</h1>
+      </div>
+      <div className="wrapper">
+        <GoBack title="Back to main page" onClick={() => navigate("/")} />
+        {/* <ProjectForm
+          projectformType="create"
+          submitText="Create Project"
+          initialValues={INIT_VAUES_PROJECTFORM}
+          schema={FullCreate}
+          onSubmit={projectFormOnSubmit}
+          disabled={savingProject}
+          buttonType="button"
+        /> */}
+      </div>
+    </div>
+  );
+}
+
+export default NewProjectPage
