bf5e21fea9e2d382ffb3f1fdd50cdabc8d97e2dd
dmojarrot <a01638460@itesm.mx>
2022-09-29 18:28:58 +0200

    Added react-quill onChange handler
    https://github.com/gtgalone/react-quilljs#with-onchange-handler

--- app/core/components/TextEditor/index.tsx
+++ app/core/components/TextEditor/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useQuill } from "react-quilljs";
 
 type TextEditorProps = {
@@ -31,8 +32,19 @@ export default function TextEditor({ defaultValue }: TextEditorProps) {
 
   const placeholder = defaultValue;
 
-  const { quillRef } = useQuill({ modules, formats, placeholder });
+  const { quill, quillRef } = useQuill({ modules, formats, placeholder });
 
+  useEffect(() => {
+    if (quill) {
+      quill.on("text-change", (delta, oldDelta, source) => {
+        console.log("Text change!");
+        console.log(quill.getText()); // Get text only
+        console.log(quill.getContents()); // Get delta contents
+        console.log(quill.root.innerHTML); // Get innerHTML using quill
+        console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
+      });
+    }
+  }, [quill]);
   return (
     <div style={{ width: "100%" }}>
       <div ref={quillRef} />
