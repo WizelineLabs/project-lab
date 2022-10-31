e38a9b5bd0566705a7fff9c1473c01dc997877d2
Jesus Corona <jesus.corona@wizeline.com>
2022-10-31 15:01:59 -0600

    test form values on submit

--- app/routes/projects/edit/$editProjectId.tsx
+++ app/routes/projects/edit/$editProjectId.tsx
@@ -13,6 +13,7 @@ import {
   getProject,
 } from "~/models/project.server";
 import type { ProjectComplete } from "~/models/project.server";
+import { getProjects } from "~/models/project.server";
 import { adminRoleName } from "app/constants";
 import type { Profiles, ProjectMembers } from "@prisma/client";
 
@@ -24,8 +25,10 @@ import {
     Grid,
     Box,
     Button,
+    Autocomplete,
+    TextField,
   } from "@mui/material";
-
+import { useState } from "react";
 
 type LoaderData = {
     isAdmin: boolean;
@@ -35,6 +38,7 @@ type LoaderData = {
     project: ProjectComplete;
     profileId: string;
     projectId: string;
+    projectsList: {id: string,name:string}[];
   };
 
 export const loader:LoaderFunction = async ({request, params}) => {
@@ -44,6 +48,11 @@ export const loader:LoaderFunction = async ({request, params}) => {
     if (!project) {
       throw new Response("Not Found", { status: 404 });
     }
+
+    const projects = await getProjects({});
+    const projectsList = projects.map((e) => {
+      return { id:e.id, name:e.name }
+    })
   
     const user = await requireUser(request);
     const profile = await requireProfile(request);
@@ -52,6 +61,7 @@ export const loader:LoaderFunction = async ({request, params}) => {
     const membership = getProjectTeamMember(profile.id, project);
     const isAdmin = user.role == adminRoleName;
     const profileId = profile.id;
+    console.log("The loader does work")
     return json<LoaderData>({
         isAdmin,
         isTeamMember,
@@ -60,11 +70,30 @@ export const loader:LoaderFunction = async ({request, params}) => {
         project,
         profileId,
         projectId,
+        projectsList
       });    
 }
 
 // ACTION
+export const action:ActionFunction = async ({request}) => {
+  const form = await request.formData();
+  try {
+      console.log(...form)
+      // console.log(selectedOptions)
+    // switch (action) {
+    //   case "EDIT_PROJECT":
+    //     const info = form.getAll();
+    //     console.log(info)
 
+        return json({ error: "" }, { status: 200 });
+    //   default: {
+    //     throw new Error("Something went wrong");
+    //   }
+    // }
+  } catch (error: any) {
+    throw error;
+  }  
+}
 
 //META
 export const meta: MetaFunction = ({ data, params }) => {
@@ -84,7 +113,12 @@ export const meta: MetaFunction = ({ data, params }) => {
 
 
 export default function EditProjectPage() {
-    const {isAdmin, isTeamMember, profile, membership, project, profileId ,projectId} = useLoaderData() as LoaderData
+    const {isAdmin, isTeamMember, profile, membership, project, profileId ,projectId, projectsList} = useLoaderData() as LoaderData
+
+    const handleChange = () => {
+      console.log("Handle the change")
+    }
+    console.log("the thing with the stuff");
     return(
         <div>
             <h1>Edit Page</h1>
@@ -101,6 +135,47 @@ export default function EditProjectPage() {
               )
             })}
             <p>{JSON.stringify(project.relatedProjects,null,2)}</p>
+
+            <div>
+              <h2>lista de projectos</h2>
+              <p>
+                {JSON.stringify(projectsList,null,2)}
+              </p>
+            </div>
+
+            <form method="POST" name="theInfo">
+            <Autocomplete
+        multiple
+        id="relatedProjects"
+        options={projectsList}
+        getOptionLabel={(option) => option.name}
+        onChange={handleChange}
+        defaultValue={project.relatedProjects}
+        filterSelectedOptions
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        // value={project.relatedProjects}
+        renderInput={(params) => (
+          <TextField
+          label="relatedProjects"
+          name="relatedProjects"
+            {...params}
+            placeholder="Related Projects"
+          />
+        )}
+      />
+      <label htmlFor="nameTest">
+        Name
+      <input type="text" name="nameTest" />
+      </label>
+
+      <label htmlFor="cartItem">
+        Items
+        <input type="text" name="cartItem[]" />
+        <input type="text" name="cartItem[]" />
+        <input type="text" name="cartItem[]" />
+      </label>      
+      <button className="primary" type="submit">SAVE</button>
+      </form>
         </div>
     )
 }
