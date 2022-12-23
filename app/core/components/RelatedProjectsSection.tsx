ba4bcc740201fbf4b7d19f53a10101d7f06d7cd4
Joaquin Bravo Contreras <jackbravo@gmail.com>
2022-12-22 18:29:46 -0600

    New related projects section component that edits in place
    Co-authored-by: Jesus Corona <jesus.corona@wizeline.com>

--- app/core/components/RelatedProjectsSection.tsx
+++ app/core/components/RelatedProjectsSection.tsx
@@ -1,26 +1,154 @@
-import { Chip, Box } from "@mui/material";
+import { useFetcher } from "@remix-run/react";
+import {
+  Autocomplete,
+  TextField,
+  Alert,
+  Chip,
+  Box,
+  IconButton,
+} from "@mui/material";
+import { EditSharp, Close } from "@mui/icons-material";
+import { useState, useEffect } from "react";
 import Link from "./Link";
 interface IProps {
   relatedProjects: any[];
+  allowEdit: Boolean;
+  projectsList: { id: string; name: string }[];
+  projectId: string;
 }
 
-function RelatedProjectsSection({ relatedProjects }: IProps) {
+function RelatedProjectsSection({
+  relatedProjects,
+  allowEdit,
+  projectsList,
+  projectId,
+}: IProps) {
+  const fetcher = useFetcher();
+  const [isEditActive, setIsEditActive] = useState(false);
+  const handleChangeEditView = (val: Boolean) => setIsEditActive(!isEditActive);
+  const [selectedRelatedProjects, setSelectedRelatedProjects] =
+    useState(relatedProjects);
+  const [error, setError] = useState<string>("");
+  const handleChange = (v: any) => {
+    setSelectedRelatedProjects(() => v);
+  };
+
+  const submitEdition = async () => {
+    try {
+      const body = {
+        ...fetcher.data,
+        projectId,
+        relatedProjects: JSON.stringify(selectedRelatedProjects),
+        action: "EDIT_RELATED_PROJECTS",
+      };
+      await fetcher.submit(body, { method: "post" });
+    } catch (error: any) {
+      console.error(error);
+    }
+    handleChangeEditView(false);
+  };
+
+  useEffect(() => {
+    //It handles the fetcher error from the response
+    if (fetcher.state === "idle" && fetcher.data) {
+      if (fetcher.data.error) {
+        setError(fetcher.data.error);
+      } else {
+        setError("");
+      }
+    }
+  }, [fetcher]);
+
+  useEffect(() => {
+    setSelectedRelatedProjects(relatedProjects);
+  }, [relatedProjects]);
+
   return (
     <>
       <big>Related Projects</big>
-      <Box>
-        {relatedProjects.map((item, i) => {
-          return (
-            <Link className="link_button" key={i} to={`/projects/${item.id}`}>
-              <Chip
-                className="chip-hover"
-                sx={{ margin: "1em .5em" }}
-                label={item?.name}
-              ></Chip>
-            </Link>
-          );
-        })}
-      </Box>
+      {/* {JSON.stringify(selectedRelatedProjects,null,2)} */}
+      {allowEdit && isEditActive ? (
+        <>
+          <IconButton
+            sx={{ margin: "1em .5em" }}
+            onClick={() => handleChangeEditView(false)}
+          >
+            <Close>Cancel</Close>
+          </IconButton>
+        </>
+      ) : (
+        <IconButton
+          sx={{ margin: "1em .5em" }}
+          onClick={() => handleChangeEditView(true)}
+        >
+          <EditSharp></EditSharp>
+        </IconButton>
+      )}
+      <div>
+        {isEditActive && (
+          <>
+            <fetcher.Form method="post">
+              <Autocomplete
+                multiple
+                id="relatedProjects"
+                options={projectsList.filter((proj) => proj.id !== projectId)}
+                getOptionLabel={(option) => option.name}
+                value={selectedRelatedProjects}
+                onChange={(event, value: { id: string; name: string }[] | []) =>
+                  handleChange(value)
+                }
+                defaultValue={relatedProjects}
+                filterSelectedOptions
+                isOptionEqualToValue={(option, value) =>
+                  option.name === value.name
+                }
+                renderInput={(params) => (
+                  <TextField
+                    name="relateProjects"
+                    label="Related Projects"
+                    {...params}
+                    placeholder="Add Related Projects..."
+                  />
+                )}
+              />
+              {error && <span>{JSON.stringify(error)}</span>}
+              <div className="margin-vertical-separator">
+                <button
+                  disabled={fetcher.state === "submitting"}
+                  className="primary"
+                  onClick={() => submitEdition()}
+                >
+                  Submit
+                </button>
+              </div>
+              {error && (
+                <Alert severity="warning">Information could not be saved</Alert>
+              )}
+            </fetcher.Form>
+          </>
+        )}
+      </div>
+
+      {!isEditActive && (
+        <Box>
+          {relatedProjects.map((item, i) => {
+            return (
+              <Link
+                className="link_button"
+                key={i}
+                to={`/projects/${item.id}`}
+                prefetch="render"
+              >
+                <Chip
+                  className="chip-hover"
+                  sx={{ margin: "1em .5em" }}
+                  label={item?.name}
+                ></Chip>
+              </Link>
+            );
+          })}
+        </Box>
+      )}
     </>
   );
 }
