5fcc6547d52215f5cda4439d1e2cc76eedf20f47
Martin Robledo <martin.robledo@wizeline.com>
2023-01-06 17:28:47 -0600

    undone all

--- app/core/components/ContributorPathReport/index.tsx
+++ app/core/components/ContributorPathReport/index.tsx
@@ -51,7 +51,101 @@ export const ContributorPathReport = ({
           </IconButton>
         )}
       </Grid>
-     
+      <table width="100%" className="table-project-members">
+        <thead>
+          <tr>
+            <th>Active</th>
+            <th>Name</th>
+            <th>Role(s)</th>
+            <th>Skills</th>
+            <th>
+              H.P.W.
+              <br />
+              <HtmlTooltip title="Hours per Week">
+                <TipBubble>?</TipBubble>
+              </HtmlTooltip>
+            </th>
+            {project.stages?.map((stage: Stage, i: number) => (
+              <th key={i}>
+                {stage.name}
+                <br />
+                {stage.projectTasks?.map(
+                  (task: ProjectTask, taskIndex: number) => (
+                    <HtmlTooltip key={taskIndex} title={task.description}>
+                      <TipBubble>{taskIndex + 1}</TipBubble>
+                    </HtmlTooltip>
+                  )
+                )}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {project.projectMembers?.map(
+            (member: ProjectMember, memberIndex: number) => {
+              const projectTaskIds: any = member.contributorPath.map(
+                (cp: ContributorPath) => cp.projectTaskId
+              );
+
+              return (
+                <tr key={memberIndex}>
+                  <td align="center">
+                    {member.active ? (
+                      <CompleteIcon>
+                        <CheckSharpIcon />
+                      </CompleteIcon>
+                    ) : (
+                      <IncompleteIcon>
+                        <ClearSharpIcon />
+                      </IncompleteIcon>
+                    )}
+                  </td>
+
+                  <td>
+                    <a href={`mailto:${member.profile.email}`}>
+                      {member.profile?.firstName} {member.profile?.lastName}
+                    </a>
+                  </td>
+
+                  <td>{member.role?.map((skill) => skill.name).join(", ")}</td>
+
+                  <td>
+                    {member.practicedSkills
+                      ?.map((skill) => skill.name)
+                      .join(", ")}
+                  </td>
+
+                  <td align="center">{member.hoursPerWeek}</td>
+                  {project.stages?.map((stage: Stage, stageIndex: number) => (
+                    <td key={stageIndex}>
+                      <ul>
+                        {stage.projectTasks?.map(
+                          (task: ProjectTask, taskIndex: number) => (
+                            <li key={taskIndex}>
+                              {projectTaskIds.includes(task.id) ? (
+                                <CompleteIcon>
+                                  <CheckBoxSharpIcon />
+                                </CompleteIcon>
+                              ) : (
+                                <>
+                                  <IncompleteIcon>
+                                    <CheckBoxOutlineBlankSharpIcon />
+                                  </IncompleteIcon>
+                                  {""}
+                                </>
+                              )}
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </td>
+                  ))}
+                </tr>
+              );
+            }
+          )}
+        </tbody>
+      </table>
     </>
   );
 };
