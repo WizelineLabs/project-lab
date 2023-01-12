aa9fd745f53f7d9a5809af1c302fb29fcc62b749
Jesus Corona <jesus.corona@wizeline.com>
2023-01-11 19:26:29 -0600

    test pre-commit validation just tsc

--- app/routes/projects/$projectId.members.tsx
+++ app/routes/projects/$projectId.members.tsx
@@ -111,6 +111,7 @@ export const action: ActionFunction = async ({ request, params }) => {
       e instanceof PrismaClientValidationError
     ) {
       console.log(e.message);
+      // console.log(e.message);
       return validationError({
         fieldErrors: {
           formError: "Server failed",
