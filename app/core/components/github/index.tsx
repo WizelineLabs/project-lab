c4b50a6eb4f8a84af13489dbb0a4d67a6e687d6b
Martin Robledo <martin.robledo@wizeline.com>
2023-01-20 14:36:05 -0600

    styling the github section

--- app/core/components/github/index.tsx
+++ app/core/components/github/index.tsx
@@ -1,11 +1,29 @@
-import { Card, CardContent, CardHeader, Container } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+  Paper,
+} from "@mui/material";
 import GitHubCommits from "./GithubCommits";
 
 export const GitHubSection = ({ repoName }: { repoName: string }) => {
   return (
     <>
       <Container sx={{ marginBottom: 2 }}>
-        <GitHubCommits repoName={repoName} />
+        <Grid container justifyContent="space-between">
+          <Grid item xs={12}>
+            <Paper>
+              <h1 style={{ marginBottom: 0 }}>Github Info</h1>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container sx={{ marginBottom: 2 }}>
+        <Grid container>
+          <GitHubCommits repoName={repoName} />
+        </Grid>
       </Container>
     </>
   );
