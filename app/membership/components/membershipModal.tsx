e55ba5d36ceb4e609c537e5fc1534cd63d5a26b9
Andresmagic <42359482+Andresmagic@users.noreply.github.com>
2023-02-10 11:02:48 -0600

    advance on the member modal

--- app/membership/components/membershipModal.tsx
+++ app/membership/components/membershipModal.tsx
@@ -1,27 +1,48 @@
-import { useState } from "react"
-import ModalBox from "../../core/components/ModalBox/index"
+import ModalBox from "../../core/components/ModalBox/index";
+import { Checkbox, FormControlLabel, TextField } from "@mui/material";
+import DisciplinesSelect from "~/core/components/DisciplinesSelect";
+import { FormDivContainer } from "~/core/components/JoinProjectModal/joinProjectModal.styles";
+import LabeledTextField from "~/core/components/LabeledTextField";
+import SkillsSelect from "~/core/components/SkillsSelect";
 
 interface IProps {
-  open: boolean
-  handleClose: React.MouseEventHandler
-  close: Function
-  label: string
-  onClick: React.MouseEventHandler
-  disabled?: boolean | false
-  className?: string | ""
+  open: boolean;
+  handleCloseModal: Function;
+  close: Function;
 }
 
-const MembershipModal = ({ ...props }: IProps) => {
+const MembershipModal = (props: IProps) => {
   return (
-    <>
-      <ModalBox open={props.open} close={props.close} handleClose={props.handleClose}>
+    <ModalBox
+      open={props.open}
+      close={props.handleCloseModal}
+      handleClose={() => {}}
+      boxStyle={{ width: "800px" }}
+    >
+      <FormDivContainer>
         <h2>
-          Hey, it seems like you haven't been involved in these projects in a while. Are you still
-          working on it?
+          Hey!, it seems like you haven't been involved in these projects in a
+          while. Are you still working on it?
         </h2>
-      </ModalBox>
-    </>
-  )
-}
 
-export default MembershipModal
+        <LabeledTextField
+          name="hoursPerWeek"
+          fullWidth
+          label="Hours per Week"
+          type="number"
+          outerProps={{ style: { marginTop: 10, marginBottom: 20 } }}
+        />
+
+        <DisciplinesSelect name="role" label="Role(s)" />
+
+        <SkillsSelect name="practicedSkills" label="Skills" />
+        <FormControlLabel
+          control={<Checkbox checked={true} />}
+          label={`Project is Active`}
+        />
+      </FormDivContainer>
+    </ModalBox>
+  );
+};
+
+export default MembershipModal;
