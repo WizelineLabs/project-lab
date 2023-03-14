162eb5b09ff6a8184092b9747d4262a6582309ac
Linda Abundis <soporte@micronics.mx>
2023-03-14 12:52:02 -0600

    migration: Update actual bar menu simple MUI components to the complex MUI component

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -14,9 +14,10 @@ import {
   Grid,
   IconButton,
   Paper,
-  Stack,
   useMediaQuery,
   useTheme,
+  AppBar,
+  Toolbar
 } from "@mui/material";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import ExpandMore from "@mui/icons-material/ExpandMore";
@@ -30,6 +31,19 @@ import type { ProjectStatus } from "~/models/status.server";
 import { getProjectStatuses } from "~/models/status.server";
 import { ongoingStage, ideaStage } from "~/constants";
 import Link from "~/core/components/Link";
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 535,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
 
 type LoaderData = {
   data: Awaited<ReturnType<typeof searchProjects>>;
@@ -95,6 +109,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     skip: ITEMS_PER_PAGE * page,
     take: ITEMS_PER_PAGE,
   });
+  // return json<LoaderData>({ data, ongoingStatuses, ideaStatuses });
   return new Response(
     JSON.stringify(
       { data, ongoingStatuses, ideaStatuses },
@@ -109,6 +124,8 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Projects() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
   const [searchParams, setSearchParams] = useSearchParams();
   const page = Number(searchParams.get("page") || 0);
 
@@ -240,6 +257,24 @@ export default function Projects() {
   const theme = useTheme();
   const lessThanMd = useMediaQuery(theme.breakpoints.down("md"));
 
+  const StyledBox = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.down('sm')]: {
+      padding: '0 16px',
+    },
+  }));
+  
+  const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    fontWeight: "bold",
+    color: theme.palette.mode === "dark" ? "#AF2E33" : "#701D21",
+    background:
+      theme.palette.mode === "dark" ? "#121212" : theme.palette.common.white,
+  }));
+
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    padding: '0 !important',
+    minHeight: '30.75px !important',
+  }));
+
   const StyledTabButton = styled(Button)(({ theme }) => ({
     fontWeight: "bold",
     color: theme.palette.mode === "dark" ? "#AF2E33" : "#701D21",
@@ -249,7 +284,7 @@ export default function Projects() {
       background: theme.palette.mode === "dark" ? "#202020" : "#F5F5F5",
     },
   }));
-
+   
   return (
     <>
       <Header title="Projects" />
@@ -257,25 +292,26 @@ export default function Projects() {
         Disable Gutters based on:
         https://stackoverflow.com/questions/70038913/materialui-show-and-hide-the-containers-gutters-based-on-breakpoints
       */}
-      <Container sx={{ marginBottom: 2 }}>
-        <Paper elevation={0} sx={{ padding: 2 }}>
-          <Stack direction="row">
-            {Object.values(tabs).map((tab) => (
-              <StyledTabButton
-                color="primary"
-                size="small"
-                disableElevation
-                variant={isTabActive(tab.name) ? "contained" : "text"}
-                onClick={() => handleTabChange(tab.name)}
-                key={tab.name}
-                sx={{ color: isTabActive(tab.name) ? "#fff" : null }}
-              >
-                {tab.title}
-              </StyledTabButton>
-            ))}
-          </Stack>
-        </Paper>
-      </Container>
+      <StyledBox sx={{maxWidth: '1200px', height:'62.75px', margin: '0 auto', mb:2, px:3}}>
+        <StyledAppBar position="static" sx={{ p:2, display:'block', borderRadius: '4px', boxShadow: 'none'}}>
+          <StyledToolbar sx={{position:'static', pl:0, height:'30.75px!important'}}>
+          {Object.values(tabs).map((tab) => (
+                <StyledTabButton
+                  size="small"
+                  disableElevation
+                  variant={isTabActive(tab.name) ? "contained" : "text"}
+                  onClick={() => handleTabChange(tab.name)}
+                  key={tab.name}
+                  sx={{
+                    color: isTabActive(tab.name) ? prefersDarkMode ? "#fff" : "#000000": null, 
+                  }}
+                >
+                  {tab.title}
+                </StyledTabButton>
+              ))}
+          </StyledToolbar>
+        </StyledAppBar>
+      </StyledBox>
       <Container>
         <Grid container spacing={2} alignItems="flex-start">
           <Grid
