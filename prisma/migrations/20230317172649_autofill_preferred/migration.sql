4b258275653d7848d36bf7c5afc1ce101e510015
Joaquin Bravo Contreras <jackbravo@gmail.com>
2023-03-18 08:36:46 -0600

    Add also preferred name to the searchCol of profiles

--- prisma/migrations/20230317172649_autofill_preferred/migration.sql
+++ prisma/migrations/20230317172649_autofill_preferred/migration.sql
@@ -16,3 +16,23 @@ CREATE TRIGGER profiles_preferred_name_trigger
   ON "Profiles"
   FOR EACH ROW
   EXECUTE FUNCTION profiles_search_col_fn();
+
+-- Fill values for Profiles.searchCol adding preferred name
+DROP TRIGGER IF EXISTS profiles_search_col_trigger ON "Profiles";
+UPDATE "Profiles" SET "searchCol" = lower(unaccent("firstName" || "preferredName" || ' ' || "lastName") || ' ' || "email");
+
+-- Trigger for Profiles.searchCol
+-- Couldn't use a generated column because unaccent is not inmutable.
+CREATE OR REPLACE FUNCTION profiles_search_col_fn() RETURNS TRIGGER
+  LANGUAGE plpgsql AS $body$
+BEGIN
+  NEW."searchCol" := lower(unaccent(NEW."firstName" || "preferredName" || ' ' || NEW."lastName") || ' ' || NEW."email");
+  RETURN NEW;
+END;
+$body$;
+
+CREATE TRIGGER profiles_search_col_trigger
+  BEFORE INSERT OR UPDATE
+  ON "Profiles"
+  FOR EACH ROW
+  EXECUTE FUNCTION profiles_search_col_fn();
