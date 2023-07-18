b9c9a68d2b958cc83319314cbc8e24b972c10187
Joaquin Bravo Contreras <jackbravo@gmail.com>
2023-07-18 12:41:59 -0600

    chore: ran prettier and moved an eslinut rule skip

--- app/utils.ts
+++ app/utils.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { useMatches } from "@remix-run/react";
 import { useMemo } from "react";
 import type { User } from "~/models/user.server";
-import type { Navigation } from "@remix-run/router"
+import type { Navigation } from "@remix-run/router";
 
 const DEFAULT_REDIRECT = "/";
 
@@ -84,6 +83,7 @@ export const isProjectMemberOrOwner = (
 };
 
 export const generateRandomNumberString = () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const crypto = require("crypto");
   const randomString = crypto.randomBytes(3).toString("hex");
 
@@ -98,4 +98,4 @@ export const validateNavigationRedirect = (navigation: Navigation) => {
     // We had a submission navigation and are now navigating to different location
     // navigation.formAction !== navigation.location.pathname
   );
-}
+};
