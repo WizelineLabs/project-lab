7dba431340499b79aa2a1e6761f3341ac3708f19
Joaquin Bravo Contreras <jackbravo@gmail.com>
2024-02-12 01:32:54 -0600

    feat: use Material Table

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,13 @@
 import { getUser } from "./session.server";
 import StylesheetUrl from "./styles/style.css";
 import { createTheme } from "./theme";
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline, useMediaQuery } from "@mui/material";
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
+import { ThemeProvider, withEmotionCache } from "@emotion/react";
+import {
+  useMediaQuery,
+  unstable_useEnhancedEffect as useEnhancedEffect,
+  CssBaseline,
+} from "@mui/material";
+import type { LoaderFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -15,37 +15,18 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 import { useMemo } from "react";
-
-export const links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: StylesheetUrl },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap",
-    },
-  ];
-};
-
-export const meta: MetaFunction = () => {
-  return [
-    {
-      title: "Wizelabs",
-    },
-    {
-      name: "description",
-      content: "Wizeline's innovation hub",
-    },
-  ];
-};
 
 export const loader: LoaderFunction = async ({ request }) => {
   return { user: await getUser(request) };
 };
 
 interface IDocumentProps {
-  children: any;
+  children: React.ReactNode;
+  title?: string;
 }
 
 export default function App() {
@@ -58,27 +39,116 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <Document>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
       </Document>
     </ThemeProvider>
   );
 }
 
-function Document({ children }: IDocumentProps) {
-  return (
-    <html lang="en" className="h-full">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">{children}</body>
-    </html>
-  );
+const Document = withEmotionCache(
+  ({ children, title }: IDocumentProps, emotionCache) => {
+    // Only executed on client
+    useEnhancedEffect(() => {
+      // re-link sheet container
+      emotionCache.sheet.container = document.head;
+      // re-inject tags
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        // eslint-disable-next-line no-underscore-dangle
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+    }, []);
+
+    return (
+      <html lang="en" className="h-full">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="description" content="Wizeline's innovation hub" />
+          {title ? <title>{title}</title> : null}
+          <Meta />
+          <Links />
+          <CssBaseline />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link rel="stylesheet" href={StylesheetUrl} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          />
+          <meta
+            name="emotion-insertion-point"
+            content="emotion-insertion-point"
+          />
+        </head>
+        <body className="h-full">
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
+
+// https://remix.run/docs/en/main/route/error-boundary
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    let message;
+    switch (error.status) {
+      case 401:
+        message = (
+          <p>
+            Oops! Looks like you tried to visit a page that you do not have
+            access to.
+          </p>
+        );
+        break;
+      case 404:
+        message = (
+          <p>Oops! Looks like you tried to visit a page that does not exist.</p>
+        );
+        break;
+
+      default:
+        throw new Error(error.data || error.statusText);
+    }
+
+    return (
+      <Document title={`${error.status} ${error.statusText}`}>
+        <h1>
+          {error.status}: {error.statusText}
+        </h1>
+        {message}
+      </Document>
+    );
+  }
+
+  if (error instanceof Error) {
+    console.error(error);
+    return (
+      <Document title="Error!">
+        <div>
+          <h1>There was an error</h1>
+          <p>{error.message}</p>
+          <hr />
+          <p>
+            Hey, developer, you should replace this with what you want your
+            users to see.
+          </p>
+        </div>
+      </Document>
+    );
+  }
+
+  return <h1>Unknown Error</h1>;
 }
