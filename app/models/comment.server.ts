c08a780ed9e2a6b80146eec4219a33d5f5ee9bdc
Joaquin Bravo Contreras <jackbravo@gmail.com>
2024-03-04 22:26:18 -0600

    feat: migrate project comments server from prisma to kysely

--- app/models/comment.server.ts
+++ app/models/comment.server.ts
@@ -1,17 +1,39 @@
-import { prisma as db } from "~/db.server";
+import { jsonArrayFrom } from "kysely/helpers/postgres";
+import { v4 as uuid } from "uuid";
+import { db } from "~/db.server";
 
 export async function getComments(projectId: string) {
-  return await db.comments.findMany({
-    where: { projectId, parentId: null },
-    orderBy: { updatedAt: "desc" },
-    include: { author: true, children: { include: { author: true } } },
-  });
+  return db
+    .selectFrom("Comments as c")
+    .leftJoin("Profiles as p", "c.authorId", "p.id")
+    .selectAll("c")
+    .select(({ eb }) => [
+      "p.preferredName as authorPreferredName",
+      "p.lastName as authorLastName",
+      "p.avatarUrl as authorAvatarUrl",
+      jsonArrayFrom(
+        eb
+          .selectFrom("Comments as c2")
+          .leftJoin("Profiles as p2", "c2.authorId", "p2.id")
+          .selectAll("c2")
+          .select([
+            "p2.preferredName as authorPreferredName",
+            "p2.lastName as authorLastName",
+            "p2.avatarUrl as authorAvatarUrl",
+          ])
+          .whereRef("c2.parentId", "=", "c.id")
+      ).as("children"),
+    ])
+    .where("c.projectId", "=", projectId)
+    .execute();
 }
 
 export async function getComment(id: string) {
-  return await db.comments.findUniqueOrThrow({
-    where: { id },
-  });
+  return await db
+    .selectFrom("Comments as c")
+    .selectAll("c")
+    .where("id", "=", id)
+    .executeTakeFirstOrThrow();
 }
 
 export async function createComment(
@@ -20,27 +42,27 @@ export async function createComment(
   body: string,
   parentId?: string | null
 ) {
-  return await db.comments.create({
-    data: {
+  return await db
+    .insertInto("Comments")
+    .values({
+      id: uuid(),
       projectId,
       authorId,
       body,
       parentId,
-    },
-  });
+    })
+    .returning(["id", "body", "authorId", "createdAt", "updatedAt"])
+    .executeTakeFirstOrThrow();
 }
 
 export async function updateComment(id: string, body: string) {
-  return await db.comments.update({
-    where: { id },
-    data: {
-      body,
-    },
-  });
+  return await db
+    .updateTable("Comments")
+    .set({ body })
+    .where("id", "=", id)
+    .executeTakeFirstOrThrow();
 }
 
 export async function deleteComment(id: string) {
-  return await db.comments.delete({
-    where: { id },
-  });
+  return await db.deleteFrom("Comments").where("id", "=", id).execute();
 }
